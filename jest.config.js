b8d5dbbf13ce4834a76b4fca2947990b56e50cf1
daylilyfield <1438647+daylilyfield@users.noreply.github.com>
2023-04-30 21:05:36 +0900

    chore: Update app name, version and etc

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  resetMocks: true,
-  restoreMocks: true,
-  // collectCoverage: true,
-  // coverageDirectory: 'coverage',
-  // coverageProvider: 'v8',
-  moduleDirectories: ['node_modules', 'src'],
-}
