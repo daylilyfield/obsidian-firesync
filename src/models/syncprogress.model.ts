9a16e1bc120e7a8c42c8e2b8790ec0e8efbc647b
daylilyfield <1438647+daylilyfield@users.noreply.github.com>
2023-05-18 23:10:18 +0900

    fix: Generate SyncProgress's ID in chronological order

--- src/models/syncprogress.model.ts
+++ src/models/syncprogress.model.ts
@@ -9,9 +9,23 @@ export type SyncProgress = {
   event: SyncEvent
 }
 
+let previousNow = performance.now().toFixed(2)
+let counter = 0
+
 export function newSyncProgress(event: SyncEvent): SyncProgress {
+  const now = performance.now().toFixed(3)
+
+  if (now === previousNow) {
+    counter++
+  } else {
+    previousNow = now
+    counter = 0
+  }
+
+  const variant = counter.toString().padStart(3, '0')
+
   return {
-    id: window.crypto.randomUUID(),
+    id: `${now}.${variant}`,
     message: event.file.path,
     progress: 0,
     done: false,
